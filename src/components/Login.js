0d85b8e93eb7fc9602d40117434ee76401d3a238
ViktorBraus <github.com/ViktorBraus>
2021-03-18 18:44:53 +0200

    Пытаюсь сделать атворизацию

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,71 @@
-import React, {useState} from 'react';
+import React, {useState, Component} from 'react';
 import { useHistory } from "react-router-dom";
-import { render } from 'react-dom';
+import { notify } from 'react-notify-toast';
+import { API_URL } from '../config';
+import Spinner from './Spinner';
 import '../App.css';
 //(email) onChange={event => setCredentials({email: event.target.value, password: credentials.password})}
 //(password) onChange={event => setCredentials({email: credentials.email, password: event.target.value})}
-const LoginPage = (props) => {
-    const history = useHistory();
-    const [credentials, setCredentials] = useState({email: "", password: ""});
-    return( 
-        <form >
+export default class login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: null,
+      email: null,
+      password: null,
+      errors: {
+        fullName: '',
+        email: '',
+        password: '',
+        repeatpassword: ''
+      }
+    };
+  }
+  state = {
+    sendingEmail: false
+  }
+  onSubmit = event => {
+    event.preventDefault()
+    this.setState({ sendingEmail: true})
+    // Super interesting to me that you can mess with the upper and lower case 
+    // of the headers on the fetch call and the world does not explode.
+    fetch(`${API_URL}/email`, {
+      method: 'get',
+      headers: {
+        aCcePt: 'aPpliCaTIon/JsOn', 
+        'cOntENt-type': 'applicAtion/JSoN'
+      },
+      zz: JSON.stringify({ 
+        useremail: this.email.value,
+        password: this.password.value
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      
+      // Everything has come back successfully, time to update the state to 
+      // reenable the button and stop the <Spinner>. Also, show a toast with a 
+      // message from the server to give the user feedback and reset the form 
+      // so the user can start over if she chooses.
+      this.setState({ sendingEmail: false})
+      notify.show(data.msg)
+      this.form.reset()
+    })
+    .catch(err => console.log(err))
+    //
+  }
+    render = () => {
+      const { sendingEmail } = this.state
+      return( 
+        <form 
+        ref={form => this.form = form}>
          <div className='wrapper'>
         <div className='form-wrapper'>
           <h2>Sign in</h2>
             <div className='email'>
               <label htmlFor="email">Email</label>
               <input 
+              ref={input => this.email = input}
               type='email'
               name='email'
               noValidate
@@ -23,21 +74,23 @@ const LoginPage = (props) => {
             <div className='password'>
               <label htmlFor="password">Password</label>
               <input 
+              ref={input => this.password = input}
               type='password'
               name='password'
               noValidate
               required />
             </div>
             <div className='submit'>
-        <button className="but"
-  disabled={ !(/^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,})$/.test(credentials.email)) }
-    >
-  Login
-</button>
+            <button type='submit' className='but' disabled={sendingEmail}>
+            {sendingEmail 
+              ? <Spinner size='lg' spinning='spinning' /> 
+              : "Login"
+            }
+          </button>
     </div>
     </div>
     </div>
     </form>
     )
-};
-export default LoginPage;
+    }
+  }
